fbbb942bd7f7956fc7bea63119a2b50edd28ae5e
ClementChaumel <clement.chaumel@gmail.com>
2022-01-28 16:25:50 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+      title: `Blandoula`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"]
+};
